54245d5d5beadede9f07ace755b031ad00cba3d6
Wabomba Bakar <56803927+Bakar-coder@users.noreply.github.com>
2020-09-24 10:59:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const handler = routes.getRequestHandler(app);
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
+const corsMiddleware  = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+
+    next();
+}
+
 app
   .prepare()
   .then(() => {
@@ -13,6 +21,7 @@ app
     server.get("*", (req, res) => handler(req, res));
     server
       .use(cors())
+      .use(corsMiddleware)
       .use(handler)
       .listen(port, (err) => {
         if (err) throw err;
