d3de6f20c8a6e7c280ddd014cbf01b2a4454f879
Wabomba Bakar <56803927+Bakar-coder@users.noreply.github.com>
2020-09-29 21:30:27 +0300

    Update env.js

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': 'http://204.48.28.184:5000',
+  'process.env.BASE_URL': 'http://localhost:5000',
   'process.env.NAMESPACE': '',
   'process.env.CLIENT_ID': ''
 }
